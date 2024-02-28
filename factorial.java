70b6199ff356cbdd72d354d67a5bbc112b83104b
Diegodepab <depablodiegouma@gmail.com>
2024-02-28 18:08:49 +0100

    refactorizacion de metodo de salida

--- factorial.java
+++ factorial.java
@@ -1,31 +1,31 @@
+import java.math.BigInteger;
 import java.util.Scanner;
 public class factorial {
-    public static long fact(long num) {
-        if (num<=1) {
-            return 1;
+    public static BigInteger fact(BigInteger num) {
+        if (num.compareTo(BigInteger.ONE)<=0) {
+            return BigInteger.ONE;
         } else {
-            return num*fact(num-1);
+            return num.multiply(fact(num.subtract(BigInteger.ONE)));
         }
     }
-    public static void main(String []args) {
-    public static long getNumber() {
+    public static BigInteger  getNumber() {
         Scanner scanInput = new Scanner(System.in);
         System.out.print("Introduzca un numero: ");
-        long num = scanInput.nextLong();
+        BigInteger num = BigInteger.valueOf(scanInput.nextLong());
         scanInput.close();
         return num;
     }
-    public static void main(String []args) {
-        long num = getNumber();
-        if (num<0) {
-            System.out.println("El factorial no esta definido"
-            + " para numeros negativos");
-            System.out.println("El factorial no esta definido" +
+	public static void showFac(BigInteger num) {
+        if (num.compareTo(BigInteger.ZERO)<0) {
+			System.out.println("El factorial no esta definido" +
             " para numeros negativos");
         } else {
-            long fac = fact(num);
+            BigInteger fac = fact(num);
             System.out.println("El factorial de "+num+" es "+fac);
         }
-        scanInput.close();
+    }
+    public static void main(String []args) {
+        BigInteger num = getNumber();
+        showFac(num);
     }
 }
